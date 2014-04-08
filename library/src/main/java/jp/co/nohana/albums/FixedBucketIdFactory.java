f1878e5e67377cada6df4efb04672ed74c01798b
KeithYokoma <Keith.Yokoma@gmail.com>
2014-04-08 19:43:15 +0900

    Add localization for a default buckets

--- library/src/main/java/jp/co/nohana/albums/FixedBucketIdFactory.java
+++ library/src/main/java/jp/co/nohana/albums/FixedBucketIdFactory.java
@@ -1,5 +1,7 @@
 package jp.co.nohana.albums;
 
+import android.content.Context;
+
 /**
  * Facade object factory of {@link jp.co.nohana.albums.BucketId}.
  * @author KeithYokoma
@@ -8,19 +10,25 @@ package jp.co.nohana.albums;
  */
 @SuppressWarnings("unused") // public API
 public enum FixedBucketIdFactory {
-    CAMERA(BucketId.valueOf("DCIM/Camera")),
-    DOWNLOAD(BucketId.valueOf("download")),
-    EDITED_ONLINE_PHOTO(BucketId.valueOf("EditedOnlinePhotos")),
-    IMPORTED_PHOTO(BucketId.valueOf("Imported")),
-    SNAP_SHOT(BucketId.valueOf("Pictures/Screenshots"));
+    CAMERA(BucketId.valueOf("DCIM/Camera"), R.string.folder_camera),
+    DOWNLOAD(BucketId.valueOf("download"), R.string.folder_download),
+    EDITED_ONLINE_PHOTO(BucketId.valueOf("EditedOnlinePhotos"), R.string.folder_edited_online_photos),
+    IMPORTED_PHOTO(BucketId.valueOf("Imported"), R.string.folder_imported),
+    SNAP_SHOT(BucketId.valueOf("Pictures/Screenshots"), R.string.folder_screenshot);
 
     private final BucketId mBucketId;
+    private final int mDisplayNameId;
 
-    private FixedBucketIdFactory(BucketId bucketId) {
+    private FixedBucketIdFactory(BucketId bucketId, int displayNameId) {
         mBucketId = bucketId;
+        mDisplayNameId = displayNameId;
     }
 
     public BucketId getBucketId() {
         return mBucketId;
+    }
+
+    public String getLocalizedDisplayName(Context context) {
+        return context.getString(mDisplayNameId);
     }
 }
